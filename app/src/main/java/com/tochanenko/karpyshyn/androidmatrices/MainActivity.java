91ef770e1afdca6cb9aaf7591f0227a6a40c433d
Olia <olga.karpyshyn@gmail.com>
2018-12-10 18:52:49 +0200

    Added A and B matrices to MainActivity. Added Spinner for operations

--- app/src/main/java/com/tochanenko/karpyshyn/androidmatrices/MainActivity.java
+++ app/src/main/java/com/tochanenko/karpyshyn/androidmatrices/MainActivity.java
@@ -2,12 +2,27 @@ package com.tochanenko.karpyshyn.androidmatrices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner operators;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Install adapter on spinner
+        operators = findViewById(R.id.operators);
+        String[] myResArray = getResources().getStringArray(R.array.operators);
+        List<String> myResArrayList = Arrays.asList(myResArray);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, R.layout.spinner_item, myResArrayList);
+        operators.setAdapter(adapter);
     }
 }
